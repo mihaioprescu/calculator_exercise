6f05a0ce78a48ebb15712d7f95f861a61e70dc61
Steffen Paul <steffen.paul@metro.digital>
2021-08-10 16:43:22 +0200

    Initial commit

---
+++ src/main/java/digital/metro/pricing/calculator/CalculatorResource.java
@@ -0,0 +1,33 @@
+package digital.metro.pricing.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.math.BigDecimal;
+
+@RestController
+public class CalculatorResource {
+
+    private BasketCalculatorService basketCalculatorService;
+
+    @Autowired
+    public CalculatorResource(BasketCalculatorService basketCalculatorService) {
+        this.basketCalculatorService = basketCalculatorService;
+    }
+
+    @PostMapping("/calculator/calculate-basket")
+    public BasketCalculationResult calculateBasket(@RequestBody Basket basket) {
+        return basketCalculatorService.calculateBasket(basket);
+    }
+
+    @GetMapping("/calculator/article/{articleId}")
+    public BigDecimal getArticlePrice(@PathVariable String articleId) {
+        return basketCalculatorService.calculateArticle(new BasketEntry(articleId, BigDecimal.ONE), null);
+    }
+
+    @GetMapping("/calculator/getarticlepriceforcustomer")
+    public BigDecimal getArticlePriceForCustomer(@RequestParam String articleId, @RequestParam String customerId) {
+        return basketCalculatorService.calculateArticle(new BasketEntry(articleId, BigDecimal.ONE), customerId);
+    }
+}
---
+++ src/main/java/digital/metro/pricing/calculator/PriceRepository.java
@@ -0,0 +1,35 @@
+package digital.metro.pricing.calculator;
+
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * A dummy implementation for testing purposes. In production, we would get real prices from a database.
+ */
+@Component
+public class PriceRepository {
+
+    private Map<String, BigDecimal> prices = new HashMap<>();
+    private Random random = new Random();
+
+    public BigDecimal getpricebyarticleId(String articleId) {
+        return prices.computeIfAbsent(articleId,
+                key -> BigDecimal.valueOf(0.5d + random.nextDouble() * 29.50d).setScale(2, RoundingMode.HALF_UP));
+    }
+
+    public BigDecimal getPriceByArticleIdAndCustomerId(String id1, String id2) {
+        switch(id2) {
+            case "customer-1":
+                return getpricebyarticleId(id1).multiply(new BigDecimal("0.90")).setScale(2, RoundingMode.HALF_UP);
+            case "customer-2":
+                return getpricebyarticleId(id1).multiply(new BigDecimal("0.85")).setScale(2, RoundingMode.HALF_UP);
+        }
+
+        return null;
+    }
+}
